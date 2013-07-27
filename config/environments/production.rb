d7ad6ebdd6a121af4913eba07b2e61fc7a06b0a3
mee213 <evans.marlo@gmail.com>
2013-07-27 13:14:07 -0500

    add devise authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Railsgirls::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # For Devise
+  # config.action_mailer.default_url_options = { :host => 'ACTUALHOSTNAMEHERE' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
